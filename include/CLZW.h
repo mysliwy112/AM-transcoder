3f3a3f3e6e9912369cb45919c8823128592084cf
mysliwy112 <112mysliwy112@wp.pl>
2020-02-10 19:13:45 +0100

    Changed copy to reference in CLZW decode function

--- include/CLZW.h
+++ include/CLZW.h
@@ -9,7 +9,7 @@
 
 using namespace std;
 
-vector<unsigned char> decodeCLZW(vector<unsigned char> code){
+vector<unsigned char> decodeCLZW(vector<unsigned char> &code){
     long long out_size=code[0]+code[1]*0x100+code[2]*0x10000+code[3]*0x1000000;
     char *output;
     output = piklib_CLZWCompression2_decompress(reinterpret_cast<char*>(code.data()), code.size());
